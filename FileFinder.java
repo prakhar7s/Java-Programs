f8b2d8d6244f59fb739cb3f4042a4d1626520009
prakhar7s <56044776+prakhar7s@users.noreply.github.com>
2020-09-30 22:51:01 +0530

    File Finder Program in Java.
    This program finds the given file using recursion.

---
+++ FileFinder.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+public class FileFinder{
+	public void findFile(String fileName, String inputPath){
+	}
+	
+	public static void main(String[] args){
+		String folderPath = System.getProperty("user.dir");
+		System.out.println(folderPath);
+	}
+}
+		
